771e4c29142c66a381e5cb4596864c7105330f7f
Jens Colman <colmanjens@gmail.com>
2019-05-01 23:24:29 +0200

    Serviceworker

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const webpush = require('web-push');
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -37,5 +39,32 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.use(bodyParser.json());
+
+const publicVapidKey = 'BHT-WtcoRmdI9-3C9dkXG-0HWnpivaGP8XgO6UW_8-tUXNFWRAoHjQ7KCXBbi7eQC-2Ml_7QyB17cxP2z8I4uoY';
+const privateVapidKey = 'MxhjAwgRocjmhe4RGM3bcoamn-O29gLY7gV42qpTdQ0';
+
+webpush.setVapidDetails('mailto:devaccjens@gmail.com', publicVapidKey, privateVapidKey);
+
+// Subscribe to route
+app.post('/subscribe', (req, res) => {
+  // Get pushSubscription object
+  const subscription = req.body;
+
+  // Send 201 - resource created
+  res.status(201).json({});
+
+  // Create payload
+  const payload = JSON.stringify({title: 'Push Test'});
+
+  // Pass object into sendNotification
+  webpush.sendNotification(subscription, payload).catch(err => console.error(err));
+});
+
+
+
+
+
 
 module.exports = app;
